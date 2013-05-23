bdc74fbbce0b38f4fe34a70b0b83d912172e986b
Steffen Gebert <steffen.gebert@typo3.org>
2013-05-23 08:43:41 +0200

    Gerrit and Gitweb cookbooks

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,6 @@
-chef_gem "chef-rewind"
-require 'chef/rewind'
 this_cookbook = "t3-gitweb"
 
 include_recipe "gitweb::default"
 
-rewind "template[/etc/gitweb.conf]" do
-  cookbook this_cookbook
-end
-
-rewind "template[#{node[:apache][:dir]}/sites-available/#{node[:gitweb][:hostname]}.conf]" do
-  cookbook this_cookbook
-end
+resources("template[/etc/gitweb.conf]").cookbook this_cookbook
+# resources("template[#{node[:apache][:dir]}/sites-available/#{node[:gitweb][:hostname]}.conf]").cookbook this_cookbook
