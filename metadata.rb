d29d84a4f93ca49ea59bf6f4832c6d8794acd3e0
Steffen Gebert <steffen.gebert@typo3.org>
2013-05-26 17:08:50 +0200

    Finally fix gitweb's SSL
    Used an old version of gitweb cookbook

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       "TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "A wrapper cookbook for gitweb"
-version          "0.1.1"
+version          "0.1.2"
 
-depends "gitweb", "= 0.1.0"
+depends "gitweb", "= 0.1.1"
