a3f5e9c9df580daa4a00cd3428a72c11ed6c300b
Steffen Gebert <steffen.gebert@typo3.org>
2013-11-18 13:13:54 +0100

    [gitweb] Add robots.txt with Crawl-Delay for MSNbot

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,10 @@ include_recipe "gitweb::default"
 
 resources("template[/etc/gitweb.conf]").cookbook this_cookbook
 # resources("template[#{node[:apache][:dir]}/sites-available/#{node[:gitweb][:hostname]}.conf]").cookbook this_cookbook
+
+file "/usr/share/gitweb/robots.txt" do
+  content <<-EOQ
+User-agent: msnbot
+Crawl-delay: 5
+EOQ
+end
