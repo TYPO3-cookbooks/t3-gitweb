9b409939c0d70858940254709049c3c452b7217f
Christian Trabold <typo3@christian-trabold.de>
2014-04-17 17:40:01 +0700

    [GLOBAL] Purge obsolete `spec` folders
    They are empty and rather useless - especially when we now introduce the
    new spec layout.
    
    They can be re-created later with
    
        rake cookbook:create_specs[cookbook_name]

--- spec/default_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'chefspec'
-
-describe 't3-gitweb::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 't3-gitweb::default' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
-  end
-end
