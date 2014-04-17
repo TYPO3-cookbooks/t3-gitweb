c59cdd58949974cecdbe0034047eaa4bf9f917cd
Christian Trabold <typo3@christian-trabold.de>
2014-04-17 13:27:37 +0700

    [GLOBAL] Cleanup `metadata.rb` for better maintainability
    This introduces the following changes:
    
    - Add missing `metadata` to `metadata.rb`
    - Fix dependencies, indentions and style to make them easier to read and
    maintain.
    
    The idea is to use `metadata.rb` as a single source of truth for our
    documentation. Thus it is very important that it can be maintained
    easily and in a consistent manner.
    
    This is a first step into this direction.
    
    See http://docs.opscode.com/config_rb_metadata.html for details on this
    file.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,8 @@ maintainer       "TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "A wrapper cookbook for gitweb"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+
 version          "0.1.6"
 
-depends "gitweb", "= 0.1.2"
+depends          "gitweb", "= 0.1.2"
