ef6fa003addb20620b1d0a1e03daa1059a3a0947
Steffen Gebert <steffen.gebert@typo3.org>
2015-08-31 21:47:58 +0200

    Bump version to 0.1.8

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          "Apache 2.0"
 description      "A wrapper cookbook for gitweb"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 
-version          "0.1.7"
+version          "0.1.8"
 
 depends          "gitweb", "= 0.1.3"
