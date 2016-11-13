f1dfc8199e9d73db87eea6178eda6c0a4a427380
Steffen Gebert <steffen.gebert@typo3.org>
2016-11-13 13:42:41 +0100

    [TASK] Drop own apache config

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,9 +2,6 @@ this_cookbook = "t3-gitweb"
 
 include_recipe "gitweb::default"
 
-resources("template[/etc/gitweb.conf]").cookbook this_cookbook
-# resources("template[#{node[:apache][:dir]}/sites-available/#{node[:gitweb][:hostname]}.conf]").cookbook this_cookbook
-
 file "/usr/share/gitweb/robots.txt" do
   content <<-EOQ
 User-agent: *
