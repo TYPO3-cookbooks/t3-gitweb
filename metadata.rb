f83b80c84ec5d36da357709ab94ca6ec842d6bc8
Steffen Gebert <steffen.gebert@typo3.org>
2017-02-10 10:17:16 +0100

    [TASK] Switch to v2 pipeline

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "A wrapper cookbook for gitweb"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-
-version          "0.2.2"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 depends          "gitweb", "~> 0.2.0"
