2690ce18ff07fa31323f04d4598b073a464019eb
Steffen Gebert <steffen.gebert@typo3.org>
2017-02-10 10:18:25 +0100

    [TASK] Cleanup default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,6 @@
-this_cookbook = "t3-gitweb"
-
 include_recipe "gitweb::default"
 
-resources("template[/etc/gitweb.conf]").cookbook this_cookbook
-# resources("template[#{node[:apache][:dir]}/sites-available/#{node[:gitweb][:hostname]}.conf]").cookbook this_cookbook
+resources("template[/etc/gitweb.conf]").cookbook cookbook_name
 
 file "/usr/share/gitweb/robots.txt" do
   content <<-EOQ
